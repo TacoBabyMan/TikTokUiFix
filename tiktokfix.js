48b35832f6c61ec1a81d67f14f66a75369c72464
Neo Sahadeo <tacobabyman@gmail.com>
2022-04-07 10:53:49 +0200

    Update 2.5
    Updated a few things. 
    Added a scale for better viewing and a few bugs.
    
    Need to do -------------------------:
    
    Background color of profile

--- tiktokfix.js
+++ tiktokfix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tiktok UI fix
 // @namespace
-// @version      2
+// @version      2.5
 // @description  Fixes the terrible UI on pc for tiktok
 // @author       Neo
 // @match        https://www.tiktok.com/*
@@ -10,15 +10,39 @@
 // ==/UserScript==
 
 (function() {
+document.querySelector('html').addEventListener('keypress',(event)=>{
+  if(event.key=="Enter"){
+    console.log("it worked;");
+  }
+});
 const applied = () =>{
-  if (document.querySelectorAll('[class*="-DivHeaderWrapperMain"]')[0].style.backgroundColor != 'black') apply();
+  if (document.querySelectorAll('[class*="-DivHeaderWrapperMain"]')[0].style.backgroundColor != 'black'){
+   apply();
+  }
 };
 
 const apply = () =>{
   document.getElementsByTagName('head')[0].getElementsByTagName('style')[0].prepend(`
-  body{
+  ::-webkit-scrollbar {
+    width:10px !important;
+  }
+  ::-webkit-scrollbar-thumb {
+    background:#888 !important;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background:#555 !important;
+  }
+
+  body, [class*="-DivLiveSection"]{
     background-color:#141414 !important;
     color:white !important;
+    margin:0px !important;
+  }
+  #loader{
+    position:fixed;
+    width:100vw;
+    height:100vh;
+    background-color:black;
   }
   [class*="-DivBodyContainer"]{
     left:0px !important;
@@ -26,10 +50,15 @@ const apply = () =>{
   }
   [class*="-DivMainContainer"]{
     position:relative !important;
-    left:50%;
+    left:20%;
     transform:translate(-50%,0%);
     padding:0px !important;
     margin:0px !important;
+  }
+
+  [class*="-DivMainContainer"]{
+    transform:scale(1.22);
+    top:77vh;
   }
 
   [class*="-DivHeaderCenterContainer"]{
@@ -42,6 +71,17 @@ const apply = () =>{
     min-width:100vw;
   }
 
+  [data-e2e="search-user-container"]{
+    opacity:0.9;
+    border-radius:16px;
+  }
+  [data-e2e="search-user-container"]:hover{
+    background-color:black !important;
+    border-radius:36px;
+    opacity:1;
+    transition:0.3s;
+  }
+
   [class*="-DivTabsContainer"]{
       display:flex;
       justify-content:center;
@@ -51,13 +91,18 @@ const apply = () =>{
     max-width:204px !important;
     padding:0px;
   }
+  [class*="-DivPanelContainer"]{
+    position:absolute;
+    margin-top:20px;
+  }
   [class*="-DivTabsContainer"]{
-    background-color:transparent !important;
+    background-color:black !important;
+    border-radius:24px;
   }
   [class*="-DivHeaderWrapperMain"], [class*="-DivScrollContainer"], [class*="-DivHeaderContainer"], [data-e2e="sug-container"]{
       background-color:black !important;
   }
-  strong, h3, h4, button[class*="-ButtonActionItem"], p, form input, [class*="-DivTab"]{
+  [data-e2e="live-side-nav-channel-title"], [data-e2e="legal-link"], [data-e2e="program-link"], [data-e2e="page-link"], [class*="-DivSideNavNickName"], [data-e2e="person-count"], span, strong, h1, h2, h3, h4, h5, h6, h7, button[class*="-ButtonActionItem"], p, form input, [class*="-DivTab"], [class*="-DivSubTitleWrapper"]{
     color:white !important;
   }
   button[data-e2e="feed-follow"]{
@@ -66,13 +111,17 @@ const apply = () =>{
   [data-e2e="nav-foryou"], [class*="-DivHeaderRightContainer"], span[data-e2e="comment-icon"], [class*="-DivActionItemContainer"], h4[data-e2e="video-music"], a[data-e2e="nav-following"], a[data-e2e="nav-live"], form span, button[data-e2e="search-button"], svg[alt="TikTok"]{
     filter:invert(100%) contrast(100%) !important;
   }
-  [data-e2e="profile-icon"], [data-e2e="like-icon"]{
+  [data-e2e="profile-icon"], [data-e2e="like-icon"], [class*="-H2MainNavText"]{
     filter:invert(1) !important;
+  }
+  [class*="-H2MainNavText"]:hover{
+    color:#ff0050 !important;
+    transition:0.3s;
   }
   [data-e2e="like-count"], [data-e2e="comment-count"], [data-e2e="share-count"], [class*="-StyledInboxIcon"], [data-e2e="inbox-title"]{
     filter:invert(1) !important;
   }
-  [class*="-DivHeaderInboxWrapper"]{
+  [class*="-DivHeaderInboxWrapper"], [class*="-StyledUploadIcon"], [class*="-StyledIcon"]{
     filter:invert(1) !important;
     background-color:black !important;
   }
@@ -82,12 +131,10 @@ const apply = () =>{
   document.querySelector('[class*="-DivScrollContainer"]').innerHTML="";
   document.querySelector('[class*="-DivScrollContainer"]').prepend(MainContent);
   document.querySelector('h2[data-e2e="recommend-video-text"]').remove();
+
 };
 
 
 window.onload=()=>{applied()};
-
-document.querySelector('html').addEventListener('keypress',()=>{});
-document.querySelector('html').addEventListener('click',()=>{});
 
 })();
